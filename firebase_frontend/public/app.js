6367ac935303648a3a9b80857a69d706ba9ce9ea
Terkwood <metaterkhorn@gmail.com>
2018-09-26 20:22:52 -0400

    Update license and provide attribution.

--- firebase_frontend/public/app.js
+++ firebase_frontend/public/app.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2018 Terkwood All Rights Reserved.
+ * This source was originally provided by Google, Inc and licensed under Apache 2.0.  Thank you.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
