fb8f842d9079390dfccdde5c20a3faf14c7aa559
Terkwood <metaterkhorn@gmail.com>
2018-10-01 18:49:48 -0400

    hack up some redis and config code

--- led_status_helper/src/main.rs
+++ led_status_helper/src/main.rs
@@ -2,6 +2,7 @@
 extern crate serde_derive;
 extern crate dotenv;
 extern crate envy;
+extern crate redis;
 
 #[derive(Deserialize, Debug)]
 struct Config {
@@ -13,11 +14,50 @@ struct Config {
     mqtt_topic: String,
 }
 
-fn main() {
-    dotenv::dotenv().expect("Unable to read .env file");
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            redis_auth: None,
+            redis_host: Some("127.0.0.1".to_string()),
+            redis_port: Some(6379),
+            mqtt_host: Some("127.0.0.1".to_string()),
+            mqtt_port: Some(1883),
+            mqtt_topic: "led_message".to_string(),
+        }
+    }
+}
 
-    match envy::from_env::<Config>() {
-        Ok(config) => println!("{:?}", config),
-        Err(e) => println!("Unable to parse config ({})", e),
+fn redis_connection_string(config: Config) -> String {
+    let auth_string = match config.redis_auth {
+        Some(a) => format!(":{}@", a),
+        None => "".to_string(),
     };
+
+    format!(
+        "redis://{}{}:{}/",
+        auth_string,
+        config
+            .redis_host
+            .unwrap_or(Config::default().redis_host.unwrap()),
+        config
+            .redis_port
+            .unwrap_or(Config::default().redis_port.unwrap())
+    )
+}
+
+fn main() {
+    dotenv::dotenv().expect("Unable to load .env file");
+
+    let config = match envy::from_env::<Config>() {
+        Ok(config) => config,
+        Err(e) => panic!("Unable to parse config ({})", e),
+    };
+
+    let redis_client = redis::Client::open(&redis_connection_string(config)[..]).unwrap();
+    let redis_conn = redis_client.get_connection().unwrap();
+    let tanks: Vec<String> = redis::cmd("SMEMBERS")
+        .arg("prawnalith/tanks")
+        .query(&redis_conn)
+        .unwrap();
+    println!("Tanks: {:?}", tanks);
 }
