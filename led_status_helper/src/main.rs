1068cf0b1ec22d093a4a5d41492f87316305fda1
Terkwood <metaterkhorn@gmail.com>
2018-10-01 17:14:29 -0400

    fmt

--- led_status_helper/src/main.rs
+++ led_status_helper/src/main.rs
@@ -5,20 +5,19 @@ extern crate envy;
 
 #[derive(Deserialize, Debug)]
 struct Config {
-  redis_auth: Option<String>,
-  redis_host: Option<String>,
-  redis_port: Option<u16>,
-  mqtt_host: Option<String>,
-  mqtt_port: Option<u16>,
-  mqtt_topic: String
+    redis_auth: Option<String>,
+    redis_host: Option<String>,
+    redis_port: Option<u16>,
+    mqtt_host: Option<String>,
+    mqtt_port: Option<u16>,
+    mqtt_topic: String,
 }
 
 fn main() {
-  
-  dotenv::dotenv().expect("Unable to read .env file");
-  
-  match envy::from_env::<Config>() {
-    Ok(config) => println!("{:?}", config),
-    Err(e) => println!("Unable to parse config ({})", e),
-};
+    dotenv::dotenv().expect("Unable to read .env file");
+
+    match envy::from_env::<Config>() {
+        Ok(config) => println!("{:?}", config),
+        Err(e) => println!("Unable to parse config ({})", e),
+    };
 }
