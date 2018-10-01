cc8c45959d87578565288817acb999bc285dfc62
Terkwood <metaterkhorn@gmail.com>
2018-10-01 16:34:37 -0400

    add serde deps

--- led_status_helper/src/main.rs
+++ led_status_helper/src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate envy;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+  redis_auth: Option<String>,
+  redis_host: Option<String>,
+  redis_port: Option<u16>,
+  mqtt_host: Option<String>,
+  mqtt_port: Option<u16>,
+  mqtt_topic: String
+}
+
 fn main() {
     println!("Hello, world!");
 }
