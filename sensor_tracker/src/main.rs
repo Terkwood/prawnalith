ff38152b1c71935e446eaa8c3adeca9a75be5348
Terkwood <metaterkhorn@gmail.com>
2018-10-02 18:34:31 -0400

    uuids and uuids and uuids

--- sensor_tracker/src/main.rs
+++ sensor_tracker/src/main.rs
@@ -26,6 +26,7 @@ struct Config {
     mqtt_port: Option<u16>,
     mqtt_topic: String,
     mqtt_keep_alive: Option<u16>,
+    sensor_id_mapping_name: Option<String>,
 }
 
 fn generate_mqtt_client_id() -> String {
@@ -43,12 +44,17 @@ fn redis_connection_string(host: &str, port: Option<u16>, auth: Option<String>) 
     format!("redis://{}{}{}", auth_string, host, port_portion)
 }
 
-fn generate_sensor_id(hex_string: &str) -> Result<Uuid, uuid::parser::ParseError> {
+/// parses a hexadecimal string  (e.g. "28654597090000e4") into
+/// a UUID v5.  the hex string is used to make a small UUID,
+/// which serves as the namespace for the resulting V5 UUID.
+/// this makes translation relatively easy between external
+/// id and internal ID.
+fn generate_sensor_id(
+    hex_string: &str,
+    mapping_name: &str,
+) -> Result<Uuid, uuid::parser::ParseError> {
     let uuid_namespace = Uuid::parse_str(&format!("0000000000000000{}", hex_string)[..])?;
-    Ok(Uuid::new_v5(
-        &uuid_namespace,
-        "temp_sensor_external_id".as_bytes(),
-    ))
+    Ok(Uuid::new_v5(&uuid_namespace, mapping_name.as_bytes()))
 }
 
 fn main() {
@@ -59,16 +65,11 @@ fn main() {
         Err(e) => panic!("Unable to parse config ({})", e),
     };
 
+    // A MASSIVE BLOCK OF DEFAULT CONFIGURATIONS LIVES HERE!
     let redis_host = &config.redis_host.unwrap_or("127.0.0.1".to_string());
     let redis_port: Option<u16> = config.redis_port;
     let redis_namespace = &config.redis_namespace.unwrap_or("".to_string());
     let redis_auth: Option<String> = config.redis_auth;
-
-    let rconn_str = redis_connection_string(redis_host, redis_port, redis_auth);
-    println!("{}", rconn_str);
-    let redis_client = redis::Client::open(&rconn_str[..]).unwrap();
-    let redis_conn = redis_client.get_connection().unwrap();
-
     let mqtt_host = &config.mqtt_host.unwrap_or("127.0.0.1".to_string());
     let mqtt_port = &config.mqtt_port.unwrap_or(1883);
     // mqtt spec states that this is measured in secs
@@ -79,6 +80,17 @@ fn main() {
         TopicFilter::new(mqtt_topic.to_string()).unwrap(),
         QualityOfService::Level0,
     );
+    let sensor_id_mapping_name = &config
+        .sensor_id_mapping_name
+        .unwrap_or("external_sensor_id".to_string());
+
+    // Set up redis client
+    let redis_client =
+        redis::Client::open(&redis_connection_string(redis_host, redis_port, redis_auth)[..])
+            .unwrap();
+    let redis_conn = redis_client.get_connection().unwrap();
+
+    // Open TCP connection to MQTT broker
     let mqtt_server_addr = format!("{}:{}", mqtt_host, mqtt_port);
     println!(
         "Opening TCP connection to MQTT server {:?} ... ",
@@ -95,8 +107,16 @@ fn main() {
             i64::from_str_radix(ext_sensor_id, 16).unwrap()
         );
         println!(
+            "external sensor id (tiny UUID) : {}",
+            Uuid::parse_str(&format!("0000000000000000{}", "28654597090000e4")[..]).unwrap()
+        );
+        println!(
+            "mapping name                   : {}",
+            sensor_id_mapping_name
+        );
+        println!(
             "internal sensor ID             : {}",
-            generate_sensor_id(ext_sensor_id).unwrap()
+            generate_sensor_id(ext_sensor_id, sensor_id_mapping_name).unwrap()
         );
     }
 }
