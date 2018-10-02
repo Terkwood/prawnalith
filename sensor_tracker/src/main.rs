4b57e595ab427056498169548de095c92d2cca5d
Terkwood <metaterkhorn@gmail.com>
2018-10-02 17:40:43 -0400

    redis deets

--- sensor_tracker/src/main.rs
+++ sensor_tracker/src/main.rs
@@ -31,6 +31,17 @@ struct Config {
     mqtt_keep_alive: Option<u16>,
 }
 
+fn redis_connection_string(host: &str, port: Option<u16>, auth: Option<String>) -> String {
+    let auth_string = match auth {
+        Some(a) => format!(":{}@", a),
+        None => "".to_string(),
+    };
+
+    let port_portion: String = port.map(|p| format!(":{}", p)).unwrap_or("".to_string());
+
+    format!("redis://{}{}:{}", auth_string, host, port_portion)
+}
+
 fn main() {
     dotenv::dotenv().expect("Unable to load .env file");
 
@@ -40,11 +51,20 @@ fn main() {
     };
 
     let redis_host = &config.redis_host.unwrap_or("127.0.0.1".to_string());
-    let redis_port = &config.redis_port.unwrap_or(6379);
+    let redis_port: Option<u16> = config.redis_port;
     let redis_namespace = &config.redis_namespace.unwrap_or("".to_string());
+    let redis_auth: Option<String> = config.redis_auth;
+
+    let redis_client =
+        redis::Client::open(&redis_connection_string(redis_host, redis_port, redis_auth)[..])
+            .unwrap();
+    let redis_conn = redis_client.get_connection().unwrap();
+
     let mqtt_host = &config.mqtt_host.unwrap_or("127.0.0.1".to_string());
     let mqtt_port = &config.redis_port.unwrap_or(1883);
-    let mqtt_keep_alive = &config.mqtt_keep_alive.unwrap_or(10); // mqtt spec states that this is measured in secs
+    // mqtt spec states that this is measured in secs
+    // see http://www.steves-internet-guide.com/mqtt-keep-alive-by-example/
+    let mqtt_keep_alive = &config.mqtt_keep_alive.unwrap_or(10);
 
     let z = i64::from_str_radix("1f", 16);
 }
