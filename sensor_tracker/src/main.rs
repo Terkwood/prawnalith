00a11eae417cf53c7574068147c702d11bce18b8
Terkwood <metaterkhorn@gmail.com>
2018-10-02 16:25:58 -0400

    basic config code for sensor tracker

--- sensor_tracker/src/main.rs
+++ sensor_tracker/src/main.rs
@@ -1,3 +1,22 @@
+#![feature(slice_concat_ext)]
+#[macro_use]
+extern crate serde_derive;
+extern crate dotenv;
+extern crate envy;
+extern crate redis;
+
+use redis::Commands;
+
+#[derive(Deserialize, Debug, Clone)]
+struct Config {
+    redis_auth: Option<String>,
+    redis_host: Option<String>,
+    redis_port: Option<u16>,
+    mqtt_host: Option<String>,
+    mqtt_port: Option<u16>,
+    mqtt_topic: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
