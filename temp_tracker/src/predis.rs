3cf5a2ff79a1e0f664616fd0ebefe4d5e05e9351
Terkwood <metaterkhorn@gmail.com>
2018-10-04 20:04:42 -0400

    a bit cleaner

--- temp_tracker/src/predis.rs
+++ temp_tracker/src/predis.rs
@@ -70,25 +70,53 @@ pub fn receive_updates(update_r: channel::Receiver<model::TempMessage>, redis_ct
                 let temp_sensor_hash_key =
                     &format!("{}/temp_sensors/{}", rn, device_id).to_string();
 
-                let assoc_tank_num: Result<Option<u16>, _> =
-                    redis_ctx.conn.hget(temp_sensor_hash_key, "tank");
+                let assoc_tank_num: Result<Vec<Option<u64>>, _> = redis_ctx
+                    .conn
+                    .hget(temp_sensor_hash_key, vec!["tank", "temp_update_count"]);
 
-                let _ = assoc_tank_num.iter().for_each(|maybe_t| {
-                    if let Some(tank_num) = maybe_t {
+                let _ = assoc_tank_num.iter().for_each(|v| {
+                    let maybe_tank_num = v.get(0).unwrap_or(&None);
+                    let maybe_temp_sensor_uc: &Option<_> = v.get(1).unwrap_or(&None);
+                    if let Some(tank_num) = maybe_tank_num {
                         // We found the tank associated with this
                         // sensor ID, so we should update that tank's
                         // current temp reading.
                         let tank_key = format!("{}/tanks/{}", rn, tank_num);
-                        let _: Result<bool, _> =
-                            redis_ctx.conn.hset(&tank_key, "temp_f", temp.temp_f);
-                        let _: Result<bool, _> =
-                            redis_ctx.conn.hset(&tank_key, "temp_c", temp.temp_c);
-                        let _: Result<bool, _> =
-                            redis_ctx.conn.hset(&tank_key, "temp_updated", epoch_secs());
+
+                        let tank_temp_count: Result<Option<u32>, _> =
+                            redis_ctx.conn.hget(&tank_key, "temp_update_count");
+
+                        let update: Result<String, _> = redis_ctx.conn.hset_multiple(
+                            &tank_key,
+                            &vec![
+                                ("temp_f", temp.temp_f.to_string()),
+                                ("temp_c", temp.temp_c.to_string()),
+                                ("temp_update_time", epoch_secs().to_string()),
+                                (
+                                    "temp_update_count",
+                                    tank_temp_count
+                                        .unwrap_or(None)
+                                        .map(|u| u + 1)
+                                        .unwrap_or(1)
+                                        .to_string(),
+                                ),
+                            ][..],
+                        );
+
+                        let _update_sensor_hits: Result<String, _> = redis_ctx.conn.hset(
+                            temp_sensor_hash_key,
+                            "temp_update_count",
+                            maybe_temp_sensor_uc.map(|u| u + 1).unwrap_or(1),
+                        );
+                        if let Err(e) = update {
+                            println!("update fails for {}: {:?}", tank_key, e);
+                        }
                     } else {
                         // We know that there's no associated "tank"
-                        // field for this key.  Let's make sure the key
-                        // itself exists.
+                        // field for this key.  Let's make sure the record
+                        // for this sensor exists -- we'll need a human
+                        // to come in and link this device to a specific tank
+                        // using redis-cli!
 
                         redis_ctx
                             .conn
@@ -100,13 +128,15 @@ pub fn receive_updates(update_r: channel::Receiver<model::TempMessage>, redis_ct
                                     let _: Result<Vec<bool>, _> = redis_ctx.conn.hset_multiple(
                                         temp_sensor_hash_key,
                                         &vec![
-                                            ("created", format!("{}", epoch_secs())),
+                                            ("create_time", format!("{}", epoch_secs())),
                                             ("ext_device_id", temp.device_id.to_string()),
                                         ][..],
                                     );
                                 }
                             });
-                    }
+                    };
+
+                    // record a hit on the temp updates
                 });
                 println!("");
             }
