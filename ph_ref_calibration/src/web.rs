bc3ba5db5b0bcc20e30551d0cfdaacfd216dab55
Terkwood <metaterkhorn@gmail.com>
2018-10-14 07:38:14 -0400

    comment

--- ph_ref_calibration/src/web.rs
+++ ph_ref_calibration/src/web.rs
@@ -3,6 +3,7 @@ struct ExtId {
     ext_id: String,
 }
 
+/// You need to Accept: text/plain in your get request
 #[get("/id?<ext_id>", format = "text/plain")]
 fn resolve_external_id(ext_id: ExtId) -> &'static str {
     "Hello, world!"
