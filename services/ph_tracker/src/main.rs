3f1a6a841efebcf476f3e6ab4cc8fe234fe44171
Terkwood <metaterkhorn@gmail.com>
2018-10-15 20:13:52 -0400

    duplicate ph_tracker into existence

--- services/ph_tracker/src/main.rs
+++ services/ph_tracker/src/main.rs
@@ -1,3 +1,35 @@
+#![feature(slice_concat_ext)]
+extern crate crossbeam_channel;
+extern crate dotenv;
+extern crate envy;
+extern crate redis;
+extern crate redis_context;
+extern crate rumqtt;
+#[macro_use]
+extern crate serde_derive;
+extern crate tracker_support;
+extern crate uuid;
+
+use std::thread;
+use std::time::Duration;
+
+use tracker_support::TrackerConfig;
+
+mod model;
+mod prawnqtt;
+mod predis;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().expect("Unable to load .env file");
+
+    let config = TrackerConfig::new();
+    let config_clone = config.clone();
+
+    let (update_s, update_r) = crossbeam_channel::bounded(5);
+
+    thread::spawn(move || predis::receive_updates(update_r, &config_clone.to_redis_context()));
+
+    let _ = tracker_support::start_mqtt(prawnqtt::create_mqtt_callback(update_s), &config);
+
+    thread::sleep(Duration::from_secs(std::u64::MAX));
 }
---
+++ services/ph_tracker/src/prawnqtt.rs
@@ -0,0 +1,31 @@
+use super::model;
+use crossbeam_channel as channel;
+use rumqtt::MqttCallback;
+
+fn deser_message(msg: &rumqtt::Message) -> Option<model::PhMessage> {
+    serde_json::from_str(std::str::from_utf8(&*msg.payload).unwrap())
+        .map(|r| Some(r))
+        .unwrap_or(None)
+}
+
+pub fn create_mqtt_callback(update_s: channel::Sender<model::PhMessage>) -> MqttCallback {
+    let deserialize_and_forward = move |msg: rumqtt::Message| {
+        println!("Message on {:?}", msg.topic);
+        let deser: Option<model::PhMessage> = deser_message(&msg);
+        match deser {
+            None => println!(
+                "\t[!] couldn't deserialize payload [!]\n\t[!]\t{:?}\t[!]",
+                msg
+            ),
+            Some(temp) =>
+            // forward the message to someone who can handle it
+            // without having to deal with sync restrictions
+            // on our local redis connection, etc
+            {
+                update_s.send(temp)
+            }
+        }
+    };
+
+    MqttCallback::new().on_message(deserialize_and_forward)
+}
