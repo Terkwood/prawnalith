c6c97446b842c3df93065dae823a867d21d2013d
Terkwood <metaterkhorn@gmail.com>
2018-10-23 23:27:01 -0400

    so stylish black

--- frontend/hud/build.sh
+++ frontend/hud/build.sh
@@ -6,5 +6,10 @@ wasm-pack build
 cp pkg/*.js .
 cp pkg/*.ts .
 cp pkg/*.wasm .
+cp index.html pkg/.
+cp index.js pkg/.
+cp package.json pkg/. # pre-printed for npm serve command to work
+cp webpack.config.js pkg/.
+cd pkg
 npm install
 npm run serve
