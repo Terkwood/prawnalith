a2548c9a012a6ddecc98762ab063be914a385f67
Terkwood <metaterkhorn@gmail.com>
2018-11-01 03:17:37 -0400

    fill holes and squish warnings

--- services/sensor_tracker/src/model.rs
+++ services/sensor_tracker/src/model.rs
@@ -8,7 +8,7 @@ pub struct SensorMessage {
     pub temp_f: Option<f64>,
     pub temp_c: Option<f64>,
     pub ph: Option<f64>,
-    pub mv: Option<f64>,
+    pub ph_mv: Option<f64>,
 }
 
 /// `external_device_id` is usually reported as a
@@ -20,8 +20,8 @@ impl SensorMessage {
             v.push(Measurement::Temp { temp_f, temp_c })
         }
 
-        if let (Some(ph), Some(mv)) = (self.ph, self.mv) {
-            v.push(Measurement::PH { ph, mv })
+        if let (Some(ph), Some(ph_mv)) = (self.ph, self.ph_mv) {
+            v.push(Measurement::PH { ph, ph_mv })
         }
 
         v
@@ -47,21 +47,29 @@ impl ExternalDeviceId {
 #[derive(Debug)]
 pub enum Measurement {
     Temp { temp_f: f64, temp_c: f64 },
-    PH { ph: f64, mv: f64 },
+    PH { ph: f64, ph_mv: f64 },
 }
 
 impl Measurement {
     pub fn name(&self) -> String {
         match self {
-            Measurement::Temp { temp_f, temp_c } => "temp".to_string(),
-            Measurement::PH { ph, mv } => "ph".to_string(),
+            Measurement::Temp {
+                temp_f: _,
+                temp_c: _,
+            } => "temp".to_string(),
+            Measurement::PH { ph: _, ph_mv: _ } => "ph".to_string(),
         }
     }
 
     pub fn to_redis(&self) -> Vec<(&str, String)> {
         match self {
-            Measurement::Temp { temp_f, temp_c } => vec![unimplemented!()],
-            Measurement::PH { ph, mv } => vec![unimplemented!()],
+            Measurement::Temp { temp_f, temp_c } => vec![
+                ("temp_f", temp_f.to_string()),
+                ("temp_c", temp_c.to_string()),
+            ],
+            Measurement::PH { ph, ph_mv } => {
+                vec![("ph", ph.to_string()), ("ph_mv", ph_mv.to_string())]
+            }
         }
     }
 }
