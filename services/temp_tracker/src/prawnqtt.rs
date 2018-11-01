8aafcc1bd4b5c903701f7b272801befbb78ac459
Terkwood <metaterkhorn@gmail.com>
2018-11-01 05:04:25 -0400

    remove temp tracker project

--- services/temp_tracker/src/prawnqtt.rs
+++
@@ -1,8 +0,0 @@
-use super::model;
-use paho_mqtt;
-
-pub fn deser_message(msg: paho_mqtt::Message) -> Option<model::TempMessage> {
-    serde_json::from_str(std::str::from_utf8(&*msg.payload()).unwrap())
-        .map(|r| Some(r))
-        .unwrap_or(None)
-}
