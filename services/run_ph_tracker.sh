06c7ac11bb09169f76b32c9012785bef827ad418
Terkwood <metaterkhorn@gmail.com>
2018-11-01 10:15:47 -0400

    correct startup scripts

--- services/run_ph_tracker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export $(grep -v '^#' .env | xargs)
-cd ../services/ph_tracker
-$PH_TRACKER_BIN
--- services/run_temp_tracker.sh
+++ services/run_temp_tracker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export $(grep -v '^#' .env | xargs)
-cd ../services/temp_tracker
-$TEMP_TRACKER_BIN
+cd services/sensor_tracker
+$SENSOR_TRACKER_BIN
