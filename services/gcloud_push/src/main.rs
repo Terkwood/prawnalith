0cd7279a7175d255c14f61e3be8ba545a39c497a
Terkwood <metaterkhorn@gmail.com>
2018-11-02 18:52:39 -0400

    doc comment

--- services/gcloud_push/src/main.rs
+++ services/gcloud_push/src/main.rs
@@ -1,3 +1,9 @@
+//! # gcloud_push
+//! 
+//! This is a service which pushes temperature and pH
+//! data to google cloud pub sub.  The temp & pH
+//! data is expected to reside in a Redis instance.
+
 fn main() {
     println!("Hello, world!");
 }
