d2cae1d9d4e914286731e3af210526234d045b66
Terkwood <metaterkhorn@gmail.com>
2018-11-02 18:57:49 -0400

    add google api

--- services/gcloud_push/src/main.rs
+++ services/gcloud_push/src/main.rs
@@ -1,5 +1,5 @@
 //! # gcloud_push
-//! 
+//!
 //! This is a service which pushes temperature and pH
 //! data to google cloud pub sub.  The temp & pH
 //! data is expected to reside in a Redis instance.
