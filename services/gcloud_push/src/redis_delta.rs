df0efcfb326eb44f8a28c946dfcb515cefe5e1ac
Terkwood <metaterkhorn@gmail.com>
2018-11-03 13:49:23 -0400

    serializations

--- services/gcloud_push/src/redis_delta.rs
+++ services/gcloud_push/src/redis_delta.rs
@@ -3,14 +3,26 @@ use super::data::Key;
 /// Represents a change to a value in Redis.
 /// Currently only supports the minimum combinations
 /// of key/value used by the prawnalith.
-///
-
-enum RDelta<'a, 'b, 'c, T> {
-    #[serde(borrow)]
-    AddSetMember(Key<'a, 'b>, RVal<'c, T>),
-    UpdateHashField(Key<'a, 'b>, RVal<'c, T>),
-    UpdateKey(Key<'a, 'b>, RVal<'c, T>),
+#[derive(Serialize, Deserialize)]
+enum RDelta<'a, 'b, 'c> {
+    AddSetMember {
+        #[serde(borrow)]
+        key: Key<'a, 'b>,
+        #[serde(borrow)]
+        rval: RVal<'c>,
+    },
+    UpdateHashField {
+        key: Key<'a, 'b>,
+        rval: RVal<'c>,
+    },
+    UpdateKey {
+        key: Key<'a, 'b>,
+        rval: RVal<'c>,
+    },
 }
 
 /// A value which is stored in Redis.
-struct RVal<'a, T>(pub &'a T);
+/// It's always represented as a string so that
+/// we can easily cope with deserialization & serialization.
+#[derive(Serialize, Deserialize)]
+struct RVal<'a>(pub &'a str);
