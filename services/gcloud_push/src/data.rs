5185b2a6cb9b0151e36659fab4eb45af184fd2c5
Terkwood <metaterkhorn@gmail.com>
2018-11-02 21:10:34 -0400

    madness

--- services/gcloud_push/src/data.rs
+++ services/gcloud_push/src/data.rs
@@ -26,40 +26,72 @@ pub enum Key {
     },
 }
 
-#[derive(Copy, Clone)]
-pub struct Namespace(pub &'static str);
+pub struct Namespace(pub String);
 
-#[derive(Copy, Clone)]
-pub struct SensorType(pub &'static str);
+pub struct SensorType(String);
+impl SensorType {
+    pub fn new(sensor_type: &str) -> SensorType {
+        SensorType(sensor_type.to_lowercase().trim().to_string())
+    }
+}
 
 /// Yields the key which allows you to access a specific
 /// record in redis.
 ///
 /// # Examples
 /// ```
-/// use gcloud_push::data::{Namespace, Key};
+/// use gcloud_push::data::{Namespace, Key, SensorType};
 /// use uuid::Uuid;
-/// 
+///
 /// let ns = Namespace("prawnspace");
 /// let all_tanks = Key::AllTanks { ns: ns };
 /// assert_eq!(all_tanks.key(), "prawnspace/tanks");
-/// 
+///
 /// let single_tank = Key::Tank { ns, id: 1 };
 /// assert_eq!(single_tank.key(), "prawnspace/tanks/1");
+///
+/// let temp_sensor = Key::Sensor { ns, st: SensorType("temp")}
 /// ```
 impl Key {
     pub fn key(&self) -> String {
         match self {
-            Key::Tank { ns, id } => format!("{}/{}", Key::AllTanks { ns: *ns }.key(), id),
-            Key::Sensor { ns, st, id } => {
-                format!("{}/{}", Key::AllSensors { ns: *ns, st: *st }.key(), id)
-            }
+            Key::Tank {
+                ns: Namespace(n),
+                id,
+            } => format!(
+                "{}/{}",
+                Key::AllTanks {
+                    ns: Namespace(n.to_string())
+                }
+                .key(),
+                id
+            ),
+            Key::Sensor {
+                ns: Namespace(n),
+                st: SensorType(st),
+                id,
+            } => format!(
+                "{}/{}",
+                Key::AllSensors {
+                    ns: Namespace(n.to_string()),
+                    st: SensorType(st.to_string())
+                }
+                .key(),
+                id
+            ),
             Key::AllTanks { ns: Namespace(n) } => format!("{}/tanks", n),
             Key::AllSensorTypes { ns: Namespace(n) } => format!("{}/sensors", n),
             Key::AllSensors {
-                ns,
+                ns: Namespace(n),
                 st: SensorType(st),
-            } => format!("{}/{}", Key::AllSensorTypes { ns: *ns }.key(), st),
+            } => format!(
+                "{}/{}",
+                Key::AllSensorTypes {
+                    ns: Namespace(n.to_string())
+                }
+                .key(),
+                st
+            ),
         }
     }
 }
