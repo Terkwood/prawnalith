d9dd707faf61ada5c409f31f5789f7dc41042c81
Terkwood <metaterkhorn@gmail.com>
2018-11-09 08:01:40 -0500

    add clone demo

--- services/redis_aggregator/examples/clone.rs
+++ services/redis_aggregator/examples/clone.rs
@@ -1,1 +1,23 @@
+extern crate dotenv;
+extern crate envy;
+extern crate redis;
+extern crate redis_aggregator;
+extern crate redis_context;
+extern crate uuid;
 
+use redis_aggregator::clone_the_world;
+use redis_aggregator::config::PubSubConfig;
+use redis_delta::REvent;
+
+fn main() {
+    dotenv::dotenv().expect("Unable to load .env file");
+
+    let config = PubSubConfig::new();
+
+    let redis_ctx = &config.to_redis_context();
+    let pubsub_ctx = &config.to_pubsub_context();
+
+    println!("Wherein We Clone the World");
+
+    clone_the_world(redis_ctx, pubsub_ctx).unwrap();
+}
