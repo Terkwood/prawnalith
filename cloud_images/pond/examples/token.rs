b982e7a720f9ac0fec48916432a871d2b20b6d4a
Terkwood <metaterkhorn@gmail.com>
2018-11-11 14:53:06 -0500

    example

--- cloud_images/pond/examples/token.rs
+++ cloud_images/pond/examples/token.rs
@@ -1,9 +1,12 @@
+// LET THE ATTRIBUTION BE KNOWN!
+// This is basically the same as what you see on https://github.com/Keats/jsonwebtoken/blob/master/README.md
+
 extern crate jsonwebtoken as jwt;
 #[macro_use]
 extern crate serde_derive;
 
-use self::jwt::{decode, encode, Algorithm, Header, Validation};
-use std::time::{Duration, SystemTime};
+use self::jwt::{decode, decode_header, encode, Algorithm, Header, Validation};
+use std::time::SystemTime;
 
 /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
 #[derive(Debug, Serialize, Deserialize)]
@@ -19,7 +22,11 @@ pub fn main() {
         company: "Norporate".to_string(),
         exp: the_future(),
     };
-    let token = encode(&Header::default(), &my_claims, "secret".as_ref()).unwrap();
+    let there = encode(&Header::default(), &my_claims, "secret".as_ref()).unwrap();
+    let _back = decode::<Claims>(&there, "secret".as_ref(), &Validation::default()).unwrap();
+
+    // In some cases, for example if you don't know the algorithm used, you will want to only decode the header:
+    let _header = decode_header(&there).unwrap();
 }
 
 fn the_future() -> usize {
