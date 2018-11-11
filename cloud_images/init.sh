d8fd1266c76042396ebb38b8b3dec18c2ef53dcc
Terkwood <metaterkhorn@gmail.com>
2018-11-11 18:31:25 -0500

    Trim init script

--- cloud_images/init.sh
+++ cloud_images/init.sh
@@ -17,7 +17,6 @@ echo alias docker-compose="'"'docker run --rm \
     docker/compose:1.13.0'"'" >> ~/.bashrc
 echo alias dc="'"'docker-compose'"'" >> ~/.bashrc
 echo alias de="'"'docker exec'"'" >> ~/.bashrc
-echo alias htop="'"'docker run --rm -it --pid host frapsoft/htop'"'" >> ~/.bashrc
 echo alias gfp="'"'git fetch && git pull'"'" >> ~/.bashrc
 echo alias gc="'"'git checkout'"'" >> ~/.bashrc
 echo alias cdw="'"'cd /var/prawnalith'"'" >> ~/.bashrc
