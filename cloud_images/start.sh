495dc36913e51708ea54f45ab47ed16f155e9fe3
Terkwood <metaterkhorn@gmail.com>
2018-11-11 12:21:38 -0500

    bash

--- cloud_images/start.sh
+++ cloud_images/start.sh
@@ -2,5 +2,5 @@
 
 sudo sysctl vm.overcommit_memory=1
 sudo sh -c 'echo never > /sys/kernel/mm/transparent_hugepage/enabled'
-/var/prawnalith/cloud_images/docker-compose.sh up
+/bin/bash /var/prawnalith/cloud_images/docker-compose.sh up
 
