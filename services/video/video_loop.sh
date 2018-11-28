a7ecee4c92c08f13770e9abc4d60af2f32302410
Terkwood <metaterkhorn@gmail.com>
2018-11-28 16:10:45 +0000

    remove old loops

--- services/video/video_loop.sh
+++ services/video/video_loop.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-raspivid -o /tmp/loop.h264 && MP4Box -add /tmp/loop.h264 /tmp/loop.mp4
+rm /tmp/loop.h264
+rm /tmp/loop.mp4
+raspivid -o /tmp/loop.h264 -t 12364 && MP4Box -add /tmp/loop.h264 /tmp/loop.mp4
