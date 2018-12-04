8ef9ea92d07dcec5d78fc685e13502c16babef89
Terkwood <metaterkhorn@gmail.com>
2018-12-04 00:35:04 +0000

    try an experiment

--- cloud_images/gstreamer/recv.sh
+++ cloud_images/gstreamer/recv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gst-launch-1.0 tcpserversrc host=0.0.0.0 port=5001 ! gdpdepay ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! x264enc ! mpegtsmux ! hlssink
+gst-launch-1.0 tcpserversrc host=0.0.0.0 port=5001 ! \
+	mpegtsmux ! hlssink
--- cloud_images/gstreamer/send.sh
+++ cloud_images/gstreamer/send.sh
@@ -14,4 +14,4 @@ PORT=8888
 
 gst-launch-1.0 -v rpicamsrc  ! videoconvert ! videoscale ! \
         video/x-raw,format=I420,width=320,height=240,framerate=15/1 ! \
-        jpegenc ! rtpjpegpay ! gdppay ! tcpclientsink host=$TARGET_IP port=$PORT
+        x264enc tune="zerolatency" threads=1 ! tcpclientsink host=$TARGET_IP port=$PORT
