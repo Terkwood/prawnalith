98fd1a40c71de56351a90301882239375eb9f718
Terkwood <metaterkhorn@gmail.com>
2018-12-03 19:31:16 -0500

    comment

--- cloud_images/gstreamer/send.sh
+++ cloud_images/gstreamer/send.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Use this script on a raspberry pi to
+# send data to a cloud instance.
+# You can use localhost as your target IP
+# if your cloud instance is 
+# protected by an SSH tunnel.
+
 TARGET_IP=localhost
 PORT=8888
 
