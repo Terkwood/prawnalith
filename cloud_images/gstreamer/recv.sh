918ebd9b9c5b3043ddf9c58f8fc8151233eda9bc
Terkwood <metaterkhorn@gmail.com>
2018-12-04 01:01:13 +0000

    weird

--- cloud_images/gstreamer/recv.sh
+++ cloud_images/gstreamer/recv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 gst-launch-1.0 tcpserversrc host=0.0.0.0 port=5001 \
-	! rtph264depay ! h264parse ! mpegtsmux ! hlssink
+	! rtph264depay ! h264parse ! avdec_h264 \
+	! x264enc ! mpegtsmux ! hlssink
