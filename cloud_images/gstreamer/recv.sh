3198f9791534643405baf106054ad435d4e77366
Terkwood <metaterkhorn@gmail.com>
2018-12-04 00:58:16 +0000

    hack recv

--- cloud_images/gstreamer/recv.sh
+++ cloud_images/gstreamer/recv.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 gst-launch-1.0 tcpserversrc host=0.0.0.0 port=5001 \
-	! application/x-rtp,clock-rate=90000,payload=96 \
-	! rtph264depay ! mpegtsmux ! hlssink
+	! rtph264depay ! h264parse ! mpegtsmux ! hlssink
