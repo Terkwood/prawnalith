2e3436f5c62a4e70c9c59bf3d900c2181dd17f83
Terkwood <metaterkhorn@gmail.com>
2018-12-04 00:57:10 +0000

    send hack

--- cloud_images/gstreamer/send.sh
+++ cloud_images/gstreamer/send.sh
@@ -12,7 +12,7 @@ PORT=8888
 # note that you can vary the framerate and width/height to easy
 # success.
 
-gst-launch-1.0 -v rpicamsrc  ! videoconvert ! videoscale ! \
-        video/x-raw,format=I420,width=320,height=240,framerate=15/1 ! \
-        x264enc tune="zerolatency" threads=1 ! h264parse ! rtph264pay pt=96 ! \
-        tcpclientsink host=$TARGET_IP port=$PORT
+gst-launch-1.0 -v rpicamsrc  ! videoconvert ! videoscale \
+        ! video/x-raw,format=I420,width=320,height=240,framerate=15/1 \
+        ! x264enc tune="zerolatency" threads=1 ! h264parse ! rtph264pay \
+	! tcpclientsink host=$TARGET_IP port=$PORT
