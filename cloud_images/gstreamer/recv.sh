6e97ecedd39a2b4b4e254f4f36d90ebf8abbed1a
Terkwood <metaterkhorn@gmail.com>
2018-12-06 22:36:41 +0000

    tune zerolatency

--- cloud_images/gstreamer/recv.sh
+++ cloud_images/gstreamer/recv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 gst-launch-1.0 tcpserversrc host=0.0.0.0 port=5001 \
-	! h264parse ! queue ! avdec_h264 ! queue ! x264enc \
+	! h264parse ! queue ! avdec_h264 ! queue \
+	! x264enc tune="zerolatency" threads=1 \
 	! queue ! mpegtsmux ! queue ! hlssink
