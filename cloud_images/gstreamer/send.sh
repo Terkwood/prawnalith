087dca371246d9aebeec3aee50f29f63aaf125bb
Terkwood <metaterkhorn@gmail.com>
2018-12-06 22:27:35 +0000

    all the queues

--- cloud_images/gstreamer/send.sh
+++ cloud_images/gstreamer/send.sh
@@ -13,7 +13,7 @@ PORT=8888
 # success.
 
 gst-launch-1.0 -v rpicamsrc \
-	! videoconvert ! videoscale \
-        ! video/x-raw,format=I420,width=320,height=240,framerate=15/1 \
-	! x264enc ! video/x-h264,stream-format=byte-stream \
+	! queue ! videoconvert ! queue ! videoscale \
+        ! queue ! video/x-raw,format=I420,width=320,height=240,framerate=15/1 \
+	! queue ! x264enc ! queue ! video/x-h264,stream-format=byte-stream \
 	! tcpclientsink host=$TARGET_IP port=$PORT
