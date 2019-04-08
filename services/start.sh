43d12bd37dcd100f1a44b309731228a4cc27d1a0
Terkwood <metaterkhorn@gmail.com>
2019-04-08 19:33:10 +0000

    it works

--- services/start.sh
+++ services/start.sh
@@ -5,4 +5,4 @@ bash ./wait-for-it.sh 0.0.0.0:1883 -- ./run_sensor_tracker.sh &
 bash ./wait-for-it.sh 0.0.0.0:36379 -- ./run_ph_ref.sh &
 # HACKY :-D
 cd ~/temp_sensor_tracker_for_test_test
-~/sensor_tracker
+bash /var/prawnalith/services/wait-for-it.sh 0.0.0.0:1883 -- ~/sensor_tracker &
