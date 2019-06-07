61267818df2cab9cf2031de4b36b45f3453e908f
Terkwood <metaterkhorn@gmail.com>
2019-06-07 00:17:26 +0000

    remove dead script

--- local_images/install_systemd_scripts.sh
+++
@@ -1,4 +0,0 @@
-# install systemd scripts
-sudo cp /var/prawnalith/local_images/systemd/*.service /etc/systemd/system/.
-cd /var/prawnalith/local_images/systemd
-for i in *.service; do [ -f "$i" ] && sudo systemctl enable $i && sudo systemctl start $i; done
