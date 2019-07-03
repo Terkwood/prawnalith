c961dddad2edae0ed6f68e987377696c1769a270
Terkwood <metaterkhorn@gmail.com>
2019-07-03 15:15:57 -0400

    weave in wifi and pubsub code

--- microcontrollers/area_dht11_esp8266/area_dht11_esp8266.ino
+++ microcontrollers/area_dht11_esp8266/area_dht11_esp8266.ino
@@ -4,27 +4,53 @@
 // TODO did we alter MQTT_KEEPALIVE ? See ph_temp_sensor_esp8266
 #include <PubSubClient.h>
 
-// WIFI SETUP
+
+// WARNING! YOU MUST REPLACE THE EXAMPLE VALUE BELOW
+const char* DEVICE_ID = "b1c8ae88-8622-415f-951e-27a21888fe19";
+
+
+// WIFI CONFIGURABLE CONSTANTS
 const char* ssid = "ssid";
 const char* password = "password";
+
+
+// MQTT CONFIGURABLE CONSTANTS
+const char* mqtt_broker = "broker";
+const int mqtt_port = 1883;
+const char* mqtt_topic  = "namespace/sensors";
+
+
+// WIFI VARS
 WiFiClient wifi_client;
 
-// MQTT SETUP
-const char* mqtt_broker = "broker";
-const char* mqtt_topic  = "namespace/sensors";
+// PUBSUB VARS
 PubSubClient mqtt_client(wifi_client);
-
 #define MQTT_MESSAGE_SIZE 128
 char mqtt_message[MQTT_MESSAGE_SIZE];
+#define MQTT_RETRY_MS 5000
 
-// HUMIDITY & TEMP SENSOR
+// HUMIDITY & TEMP SENSOR VARS
 DHTesp dht;
 
 const int MEASUREMENT_FREQ_MS = dht.getMinimumSamplingPeriod() * 2;
 
-void setup()
-{
-  Serial.begin(115200);
+void setup_wifi(void) {
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(333);
+    Serial.println("...waiting for Wi-Fi connection...");
+  }
+
+  Serial.println("");
+  Serial.print("Wireless network SSID: ");
+  Serial.println(ssid);
+  Serial.print("Local IP address:      ");
+  Serial.println(WiFi.localIP());
+  Serial.println("");
+}
+
+void setup_dht_sensor(void) {
   Serial.println();
   Serial.println(ARDUINO_BOARD);
   Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
@@ -33,7 +59,49 @@ void setup()
   dht.setup(D4, DHTesp::DHT11); // Connect DHT sensor to GPIO 4
 }
 
-void loop()
+void setup_mqtt(void) {
+  // We'll need to randomly generate a client ID later.
+  randomSeed(micros());
+
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+}
+
+
+// Thanks to https://github.com/knolleary/pubsubclient/blob/master/examples/mqtt_esp8266/mqtt_esp8266.ino
+void connect_mqtt(void) {
+  // Loop until we're connected
+  while (!mqtt_client.connected()) {
+    Serial.print("Connecting to MQTT broker...");
+    // Create a random client ID
+    String clientId = "dht11_";
+    clientId += String(random(0xffff), HEX);
+
+    // Attempt to connect
+    if (mqtt_client.connect(clientId.c_str())) {
+      Serial.println("MQTT connected");
+    } else {
+      Serial.print("MQTT connection failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" ...trying again in 5 seconds");
+
+      delay(MQTT_RETRY_MS);
+    }
+  }
+}
+
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  setup_mqtt();
+
+  setup_dht_sensor();
+}
+
+void loop(void)
 {
   delay(MEASUREMENT_FREQ_MS);
 
@@ -56,5 +124,26 @@ void loop()
   Serial.print("\t\t");
   Serial.print(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
   Serial.println(" H%F");
+
+  
+  if (!mqtt_client.connected()) {
+    connect_mqtt();
+  }
+  mqtt_client.loop();
+
+  // publish formatted message to MQTT topic
+  snprintf(
+    mqtt_message,
+    MQTT_MESSAGE_SIZE,
+    "{ \"device_id\": \"%s\", \"temp_c\": %.2f, \"temp_f\": %.2f, \"humidity\": %.2f, \"heat_index_c\": %.2f, \"heat_index_f\": %.2f }",
+    DEVICE_ID,  // snprintf wants a const char*
+    temperature,
+    dht.toFahrenheit(temperature),
+    humidity,
+    dht.computeHeatIndex(temperature, humidity, false),
+    dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true)
+  );
+  
+  mqtt_client.publish(mqtt_topic, mqtt_message);
 } 
 
