dfd4300de41c165334245e5528550a85dc5dfbe0
Wandering Prawn Monger <metaterkhorn@gmail.com>
2019-07-24 15:48:01 +0000

    update build

--- services/sensor_tracker/build.sh
+++ services/sensor_tracker/build.sh
@@ -3,4 +3,4 @@
 # see https://ownyourbits.com/2018/06/27/running-and-building-arm-docker-containers-in-x86/
 cp /usr/bin/qemu-arm-static .
 
-docker build -f Dockerfile.test -t sensor-tracker-armhf:testing
+docker build . -f Dockerfile.test -t sensor-tracker-armhf:testing
