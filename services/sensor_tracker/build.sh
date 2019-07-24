35684946f9d70e805ecd57f017bc2974a4f76acf
Terkwood <metaterkhorn@gmail.com>
2019-07-24 11:21:14 -0400

    hack together dockerfiles

--- services/sensor_tracker/build.sh
+++ services/sensor_tracker/build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+# see https://ownyourbits.com/2018/06/27/running-and-building-arm-docker-containers-in-x86/
+cp /usr/bin/qemu-arm-static .
+
+docker build -f Dockerfile.test -t sensor-tracker-armhf:testing
