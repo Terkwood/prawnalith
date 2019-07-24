0116ca4b28f143c50a5268d8bf78af7d9d05f624
Terkwood <metaterkhorn@gmail.com>
2019-07-24 14:38:46 -0400

    move file

--- services/sensor_tracker/build_qemu_x86.sh
+++ services/sensor_tracker/build_qemu_x86.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# ...⚠️ this tends to fail with Signal 11 error ⚠️...
+
 # see https://ownyourbits.com/2018/06/27/running-and-building-arm-docker-containers-in-x86/
 # see https://matchboxdorry.gitbooks.io/matchboxblog/content/blogs/build_and_run_arm_images.html
 cp /usr/bin/qemu-arm-static .
 
 docker build . -f Dockerfile.test -t prawnalith/sensor_tracker:test
-docker build . -f Dockerfile.prod -t prawnalith/sensor_tracker:prod
+docker build . -f Dockerfile.qemu_trim -t prawnalith/sensor_tracker:prod
 
 # reclaim the 5MB used by qemu-arm-static 😈
 docker build --squash . -t prawnalith/sensor_tracker:prod
