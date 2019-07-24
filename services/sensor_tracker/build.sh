bd03ff3e28ebbc8b2feb55cfd853c649d3f1348a
Terkwood <metaterkhorn@gmail.com>
2019-07-24 12:19:22 -0400

    add link

--- services/sensor_tracker/build.sh
+++ services/sensor_tracker/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # see https://ownyourbits.com/2018/06/27/running-and-building-arm-docker-containers-in-x86/
+# see https://matchboxdorry.gitbooks.io/matchboxblog/content/blogs/build_and_run_arm_images.html
 cp /usr/bin/qemu-arm-static .
 
 docker build -f Dockerfile.test -t sensor-tracker-armhf:testing
