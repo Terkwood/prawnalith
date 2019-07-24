decdd3a2a6507400ac37f7d32e847c16b6177d8c
Terkwood <metaterkhorn@gmail.com>
2019-07-24 18:46:52 -0400

    add rumqtt options

--- services/sensor_tracker/src/prawnqtt.rs
+++ services/sensor_tracker/src/prawnqtt.rs
@@ -23,6 +23,13 @@ pub fn start_mqtt(config: &TrackerConfig) -> (Receiver<Option<Message>>, Client)
     let server_uri = format!("tcp://{}:{}", host, port);
     let server_uri_print = server_uri.clone();
 
+
+    let reconnection_options = ReconnectOptions::Always(10);
+    let mqtt_options = MqttOptions::new("test-pubsub2", broker, port)
+                                    .set_keep_alive(10)
+                                    .set_reconnect_opts(reconnection_options)
+                                    .set_clean_session(false);
+
     (unimplemented!(), unimplemented!())
 }
 
