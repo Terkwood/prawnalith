decabdf6f7b84b9c9320c0cb9107f8f0256e9a7e
Terkwood <metaterkhorn@gmail.com>
2019-07-25 11:34:55 -0400

    trim

--- services/sensor_tracker/src/prawnqtt.rs
+++ services/sensor_tracker/src/prawnqtt.rs
@@ -1,11 +1,6 @@
-use super::model;
-
-use super::config::TrackerConfig;
-use crate::model::SensorMessage;
+use crate::config::TrackerConfig;
 use crossbeam::Receiver;
-use rumqtt::{Message, MqttClient, MqttOptions, Notification, Publish, QoS, ReconnectOptions};
-use std::sync::Arc;
-use std::{thread, time::Duration};
+use rumqtt::{MqttClient, MqttOptions, Notification, QoS, ReconnectOptions};
 use uuid::Uuid;
 
 pub fn start_mqtt(config: &TrackerConfig) -> Receiver<Notification> {
@@ -17,9 +12,6 @@ pub fn start_mqtt(config: &TrackerConfig) -> Receiver<Notification> {
     let keep_alive = &config.mqtt_keep_alive.unwrap_or(10);
     let topic = &config.mqtt_topic;
     let qos = &config.mqtt_qos.unwrap_or(1);
-
-    let server_uri = format!("tcp://{}:{}", host, port);
-    let server_uri_print = server_uri.clone();
 
     let reconnection_options = ReconnectOptions::Always(10);
     let mqtt_options = MqttOptions::new(generate_mq_client_id(), host, *port)
@@ -34,53 +26,6 @@ pub fn start_mqtt(config: &TrackerConfig) -> Receiver<Notification> {
         .unwrap();
 
     notifications
-}
-
-fn DEAD_start_paho_mqtt(
-    config: &TrackerConfig,
-) -> (std::sync::mpsc::Receiver<Option<Message>>, MqttClient) {
-    unimplemented!();
-    /*
-    // Create the client. Use an ID for a persisten session.
-    // A real system should try harder to use a unique ID.
-    let create_opts = paho_mqtt::CreateOptionsBuilder::new()
-        .server_uri(server_uri)
-        .client_id(generate_mq_client_id())
-        .finalize();
-    
-    let mut cli = paho_mqtt::Client::new(create_opts).expect("Error creating the MQTT client");
-    
-    // Define the set of options for the connection
-    let will = paho_mqtt::MessageBuilder::new()
-        .topic(topic.to_string())
-        .payload("Sync consumer lost connection")
-        .finalize();
-    
-    let conn_opts = paho_mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(std::time::Duration::from_secs(*keep_alive as u64))
-        .clean_session(false)
-        .will_message(will)
-        .finalize();
-    
-    // Make the connection to the broker
-    println!("Connecting to the MQTT broker at {}", server_uri_print);
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Error connecting to the broker: {:?}", e);
-        std::process::exit(1);
-    };
-    
-    // Initialize the consumer & subscribe to topics
-    println!("Subscribing to topic {}", topic);
-    let rx = cli.start_consuming();
-    
-    if let Err(e) = cli.subscribe_many(&[topic], &[*qos as i32]) {
-        println!("Error subscribing to topics: {:?}", e);
-        cli.disconnect(None).unwrap();
-        std::process::exit(1);
-    };
-    
-    (rx, cli)
-    */
 }
 
 fn generate_mq_client_id() -> String {
