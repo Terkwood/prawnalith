bd8267987969f5a85729b02f108c0ecb8d7d7f15
Terkwood <metaterkhorn@gmail.com>
2020-03-20 21:46:24 +0000

    Use flexible var

--- services/video/send_stream_udp_local.sh
+++ services/video/send_stream_udp_local.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-TARGET_IP=192.168.1.1
 
-gst-launch-1.0 -v rpicamsrc  ! videoconvert ! videoscale ! video/x-raw,format=I420,width=640,height=480,framerate=20/1 ! jpegenc ! rtpjpegpay ! udpsink host=$TARGET_IP port=5001
+gst-launch-1.0 -v rpicamsrc  ! videoconvert ! videoscale ! video/x-raw,format=I420,width=640,height=480,framerate=20/1 ! jpegenc ! rtpjpegpay ! udpsink host=${TARGET_IP:="192.168.1.1"} port=5001
